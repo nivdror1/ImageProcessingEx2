02183035653e54ff8c7dc976ebda7dd18cd66c4d
nivdror1 <nivdror1@gmail.com>
2017-11-11 15:46:59 +0200

    something wrong it the dft2

--- sol2.py
+++ sol2.py
@@ -1,24 +1,25 @@
 import numpy as np
 import scipy.signal as sig
+from scipy.misc import imread as imread
+import matplotlib.pyplot as plt
 
 
-def euler_formula(signal, u, sign):
+def exp_formula(shape, u, arr, sign):
     '''
-    compute euler formula
-    :param signal: a vector that encapsulates a row in a matrix
+    compute the exp formula
+    :param shape: the shape of the array
     :param u:
     :param sign: A boolean variable, where true means dft and false means idf
     :return: return a matrix that contains the calculation
     '''
     #compute the input to euler formula
     if sign:
-        pre_input = np.divide(((-2*np.pi)*u), signal.shape)
+        pre_input = np.divide(((-2j*np.pi)*u), shape)
     else:
-        pre_input = np.divide(((2 * np.pi) * u), signal.shape)
-    euler_input =  signal *pre_input
+        pre_input = np.divide(((2j * np.pi) * u), shape)
+    exp_input =  arr *pre_input
     #return the calculation euler formula
-    return np.cos(euler_input) + (np.sin(euler_input)*1j)
-
+    return np.exp(exp_input)
 
 def DFT(signal):
     '''
@@ -28,13 +29,13 @@ def DFT(signal):
     '''
     arr = np.arange(signal.shape[0])
     u = arr.reshape((signal.shape[0], 1))
-    # compute the fft matrix using euler formula
-    fft_matrix = euler_formula(signal, u, True)
+    # compute the fft matrix using exp formula
+    fft_matrix = exp_formula(signal.shape, u,arr , True)
     #multiply the fft matrix with the vector of the signal
     return np.dot(fft_matrix, signal)
 
 
-def Idft(fourier_signal):
+def IDFT(fourier_signal):
     '''
     compute the inverse discrete fourier transform
     :param fourier_signal: a vector that encapsulates a row in a matrix
@@ -43,19 +44,63 @@ def Idft(fourier_signal):
     arr = np.arange(fourier_signal.shape[0])
     u = arr.reshape((fourier_signal.shape[0], 1))
     # compute the fft matrix using euler formula
-    fft_matrix = euler_formula(fourier_signal, u, False)
+
+    fft_matrix = exp_formula(fourier_signal.shape, u, arr, False)
     # multiply the inverse fft matrix with the vector of the signal
     iFourier_array = np.dot(fft_matrix, fourier_signal)
+
     #divide by the length of the fourier signal
-    return np.divide(iFourier_array,fourier_signal.shape)
+    normalized_idft = np.divide(iFourier_array,fourier_signal.shape)
+    #todo decide if the origin array is real maybe use global variable
+    #ignore the imaginary values
+    #return np.real(normalized_idft)
+    return normalized_idft
+
+def dft_image(image ):
+    fourier_image = np.asarray(image).astype(np.complex128)
+    for row in range(image.shape[0]):
+        fourier_image[row, :] = DFT(image[row, :])
+    return fourier_image
+
+
+def idft_image(fourier_image):
+    img = np.asarray(fourier_image).astype(np.float64)
+    for row in range(fourier_image.shape[0]):
+        img[row, :] = IDFT(fourier_image[row, :])
+    return img
+
+def DFT2(image):
+    arr = np.arange(image.shape[0])
+    u = arr.reshape((image.shape[0], 1))
+    fourier_image = dft_image(image)
+    fft_matrix = exp_formula(image.shape[0], u, arr, True)
+    return np.dot(fft_matrix, fourier_image)
+
+
+def IDFT2(fourier_image):
+    arr = np.arange(fourier_image.shape[0])
+    u = arr.reshape((fourier_image.shape[0], 1))
+    img = idft_image(fourier_image)
+
+    fft_matrix = exp_formula(fourier_image.shape[0], u, arr, False)
+
+    ifourier_matrix= np.divide(np.dot(fft_matrix, img),fourier_image.shape[0])
+    return np.real(ifourier_matrix)
+
 
 
 def main():
-    a= [3.5,5.6, 4.6]
-    signal = np.array(a).astype(np.float64)
-    fourier= DFT(signal)
-    check_fourier = np.fft.fft(a)
-    f=4
+    name = "logoGray.jpg"
+    img = imread(name)
+    a= [[1.2, 2.4],[5.0, 7.1], [6.5, 3.4]]
+    a=np.asarray(a).astype(np.float64)
+    # fourier = np.fft.fft2(img)
+    # ifourier = np.fft.ifft2(fourier).astype(np.uint8)
+    # plt.imshow(ifourier)
+    f= DFT2(a)
+    i_f = IDFT2(f)
+    v=4
+
 
 if __name__ == "__main__":
     main()
